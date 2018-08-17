765c4e64d518388eaa773ff43e2625982ab13300
zhurunshi <rushzhu95@hotmail.com>
2018-08-17 08:39:34 +0800

    学习完46-持有对象_容器类基本概念和打印

--- src/pers/rush/beginner/_46_持有对象_容器类基本概念和打印/PrintingContainers.java
+++ src/pers/rush/beginner/_46_持有对象_容器类基本概念和打印/PrintingContainers.java
@@ -4,20 +4,24 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class PrintingContainers {
     
     public static void main(String[] args) {
-        System.out.println(fill(new ArrayList<String>()));
-        System.out.println(new LinkedList<String>());
-        System.out.println(new HashSet<String>());
-        System.out.println(new TreeSet<String>());
-        System.out.println(new LinkedHashSet<String>());
-        System.out.println(new HashMap<String, String>());
+        System.out.println("ArrayList: " + fill(new ArrayList<String>()));
+        System.out.println("LinkedList: " + fill(new LinkedList<String>()));
+        System.out.println("HashSet: " + fill(new HashSet<String>()));
+        System.out.println("TreeSet: " + fill(new TreeSet<String>()));
+        System.out.println("LinkedHashSet: " + fill(new LinkedHashSet<String>()));
+        System.out.println("HashMap: " + fill(new HashMap<String, String>()));
+        System.out.println("TreeMap: " + fill(new TreeMap<String, String>()));
+        System.out.println("LinkedHashMap: " + fill(new LinkedHashMap<String, String>()));
     }
     
     static Collection fill(Collection<String> collection) {
@@ -33,7 +37,7 @@ public class PrintingContainers {
         map.put("ret", "fuzzy"); // 老鼠，有毛的
         map.put("cat", "rags"); // 可以嬉闹玩耍的
         map.put("dog", "bosco");
-        map.put("dog", "spot"); // 侦查
+        map.put("dog", "spot"); // 侦查,可以把前面的值覆盖掉
         return map;
     }
         
