0b5f27a193d3274f3f07162f0125bb36f14a2c3b
Rush-PC <rushzhu95@hotmail.com>
2018-09-09 23:05:17 +0800

    学习到56-通过异常处理错误_捕获所有异常

---
+++ src/pers/rush/beginner/_56_通过异常处理错误_捕获所有异常/ExceptionMethods.java
@@ -0,0 +1,13 @@
+package pers.rush.beginner._56_通过异常处理错误_捕获所有异常;
+
+public class ExceptionMethods {
+
+    public static void main(String[] args) {
+        /*
+         * 1.栈轨迹
+         * 2.重新抛出异常
+         * 3.异常链
+         */
+    }
+
+}
